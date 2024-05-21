ea5d398fd79617675be952f5226cb3b771af160d
mouadedd <mdx.edd@gmail.com>
2024-05-21 07:29:49 -0700

    test checker

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """utf-8 validation"""
-
+# 0-127
+# 192 -223
+# 224-239
+# 240 -247
 
 def validUTF8(data):
     result = True
     for i in data:
-        if i > 247:
+        if i > 127:
             result = False
             break
     return result
