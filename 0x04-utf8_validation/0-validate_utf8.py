3ad11edde5ded1aa0b2a06594f88c0b174b473f3
mouadedd <mdx.edd@gmail.com>
2024-05-23 04:10:27 -0700

    comment functions

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,7 +9,9 @@
 
 
 def validUTF8(data):
+    """function to check if data is utf-8 valid"""
     def check_sequence(index, num_bytes):
+        """checking the sequence fall range"""
         for i in range(1, num_bytes):
             if index + i >= len(data) or not (128 <= data[index + i] <= 191):
                 return False
@@ -18,23 +20,23 @@ def validUTF8(data):
     index = 0
     while index < len(data):
         byte = data[index]
-        if byte < 128:  # 1-byte sequence
+        if byte < 128:
             index += 1
-        elif 192 <= byte <= 223:  # 2-byte sequence
+        elif 192 <= byte <= 223:
             if check_sequence(index, 2):
                 index += 2
             else:
                 return False
-        elif 224 <= byte <= 239:  # 3-byte sequence
+        elif 224 <= byte <= 239:
             if check_sequence(index, 3):
                 index += 3
             else:
                 return False
-        elif 240 <= byte <= 247:  # 4-byte sequence
+        elif 240 <= byte <= 247:
             if check_sequence(index, 4):
                 index += 4
             else:
                 return False
         else:
-            return False  # Invalid leading byte
+            return False
     return True
