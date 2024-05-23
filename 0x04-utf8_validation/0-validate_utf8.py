bdc83f5140d47c9423b6b4e439d60bb6f8ba307a
mouadedd <mdx.edd@gmail.com>
2024-05-23 04:39:12 -0700

    change the code to use bits instead of data ranges

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,33 +10,22 @@
 
 def validUTF8(data):
     """function to check if data is utf-8 valid"""
-    def check_sequence(index, num_bytes):
-        """checking the sequence fall range"""
-        for i in range(1, num_bytes):
-            if index + i >= len(data) or not (128 <= data[index + i] <= 191):
-                return False
-        return True
-
     index = 0
-    while index < len(data):
-        byte = data[index]
-        if byte < 128:
-            index += 1
-        elif 192 <= byte <= 223:
-            if check_sequence(index, 2):
-                index += 2
-            else:
+
+    for i in data:
+        if index == 0:
+            if i >> 7 == 0b1:
                 return False
-        elif 224 <= byte <= 239:
-            if check_sequence(index, 3):
-                index += 3
-            else:
-                return False
-        elif 240 <= byte <= 247:
-            if check_sequence(index, 4):
-                index += 4
-            else:
-                return False
+            elif i >> 5 == 0b110 or i >> 5 == 0b1110:
+                index = 1
+            elif i >> 4 == 0b1110:
+                index = 2
+            elif i >> 3 == 0b11110:
+                index = 3
+
         else:
+            if i >> 6 == 0b10:
+                index -= 1
             return False
-    return True
+
+    return index == 0
