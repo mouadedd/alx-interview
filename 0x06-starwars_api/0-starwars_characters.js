bc4a051fba537d28eb313c60f19eb8d5fe97768d
mouadedd <mdx.edd@gmail.com>
2024-06-06 12:17:35 -0700

    test

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// 0. Star Wars Characters
+
+const request = require('request');
+const film = process.argv[2];
+const api = 'https://swapi-api.hbtn.io/api/';
+const url = api + 'films/' + film + '/';
+request.get({ url: url }, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    order(characters);
+  }
+});
+
+function order (characters) {
+  if (characters.length > 0) {
+    request.get({ url: characters.shift() }, function (error, response, body) {
+      if (!error) {
+        console.log(JSON.parse(body).name);
+        order(characters);
+      }
+    });
+  }
+}
